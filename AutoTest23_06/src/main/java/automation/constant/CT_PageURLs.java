35159ffd638d13daa436ffa1dcc1e87551414f49
tuananh651 <164884441+tuananh651@users.noreply.github.com>
2024-08-13 23:57:58 +0700

    Add files via upload

--- AutoTest23_06/src/main/java/automation/constant/CT_PageURLs.java
+++ AutoTest23_06/src/main/java/automation/constant/CT_PageURLs.java
@@ -1,5 +1,10 @@
 package automation.constant;
 
 public class CT_PageURLs {
+	
+	public static String URL_SELENIUMEASY="https://demo.seleniumeasy.com/basic-checkbox-demo.html";
+	public static String URL_SELENIUMEASY_2="https://demo.seleniumeasy.com/basic-radiobutton-demo.html";
+	public static String URL_SELENIUMEASY_3="https://demo.seleniumeasy.com/basic-select-dropdown-demo.html";
+	public static String URL_SELENIUMEASY_4="http://demo.seleniumeasy.com/input-form-demo.html";
 
 }
