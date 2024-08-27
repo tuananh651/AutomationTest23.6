b95e5420b8bb4aa1a6b6ad5efb70bb7c15245155
tuananh651 <164884441+tuananh651@users.noreply.github.com>
2024-08-28 00:31:00 +0700

    Add files via upload

---
+++ AutoTest23_06/src/main/java/automation/testsuite/Day13_BTVN_loginTest.java
@@ -0,0 +1,63 @@
+package automation.testsuite;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import automaiton.common.CommonBase;
+import automation.constant.CT_PageURLs;
+import automation.page.Day13_BTVN_loginPage;
+
+public class Day13_BTVN_loginTest extends CommonBase
+{
+	Day13_BTVN_loginPage login;
+	
+	@BeforeMethod
+	public void openBrowser()
+	{
+		driver = initFirefoxDriver(CT_PageURLs.URL_CODESTAR);
+	}
+	
+	@Test
+	public void loginSuccessfully()
+	{
+		login = new Day13_BTVN_loginPage(driver);
+		login.Function("admin@gmail.com", "12345678");
+		assertTrue(driver.findElement(By.xpath("//p[text()='Quản lý người dùng']")).isDisplayed());
+	}
+	
+	@Test
+	public void loginFail_EmailNotExist()
+	{
+		login = new Day13_BTVN_loginPage(driver);
+		login.Function("login@gmail.com", "12345678");
+		assertTrue(driver.findElement(By.xpath("//p[text()='Đăng nhập để truy cập vào hệ thống kho hàng']")).isDisplayed());
+	}
+	
+	@Test
+	public void loginFail_PasswordNotExist()
+	{
+		login = new Day13_BTVN_loginPage(driver);
+		login.Function("admin@gmail.com", "12345623");
+		assertTrue(driver.findElement(By.xpath("//p[text()='Đăng nhập để truy cập vào hệ thống kho hàng']")).isDisplayed());
+	}
+	
+	@Test
+	public void loginFail_EmailAndPasswordNotExist()
+	{
+		login = new Day13_BTVN_loginPage(driver);
+		login.Function("login@gmail.com", "12345623");
+		assertTrue(driver.findElement(By.xpath("//p[text()='Đăng nhập để truy cập vào hệ thống kho hàng']")).isDisplayed());
+	}
+	
+	
+	@AfterMethod
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
+}
