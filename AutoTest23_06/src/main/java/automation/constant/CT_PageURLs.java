84e0528a187ef1256fe6bedba86c461b93e82b0b
tuananh651 <164884441+tuananh651@users.noreply.github.com>
2024-09-15 16:15:03 +0700

    Add files via upload

--- AutoTest23_06/src/main/java/automation/constant/CT_PageURLs.java
+++ AutoTest23_06/src/main/java/automation/constant/CT_PageURLs.java
@@ -8,5 +8,9 @@ public class CT_PageURLs {
 	public static String URL_SELENIUMEASY_4="http://demo.seleniumeasy.com/input-form-demo.html";
 	public static String URL_ALADA="https://alada.vn/";
 	public static String URL_CODESTAR="http://test-system.crmstar.vn/";
+	public static String URL_TEDU="https://tedu.com.vn/dang-nhap.html";
+	public static String URL_GURU="https://demo.guru99.com/test/";
+	public static String URL_AUTOMATIONTESTING = "http://demo.automationtesting.in/Alerts.html";
+	public static String URL_RISE = "https://rise.fairsketch.com/signin";
 
 }
